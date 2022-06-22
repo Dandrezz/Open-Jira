22b3230d7effa7824fff4a1936fffa55f177e226
Dandrezz <diegoaporterol@gmail.com>
2022-06-21 23:20:51 -0500

    Navbar module

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,14 +1,19 @@
 import { MenuOutlined } from '@mui/icons-material'
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import React, { useContext } from 'react'
+import { UIContext } from '../../context/ui'
 
 export const Navbar = () => {
+
+  const {openSideMenu} = useContext(UIContext)
+
   return (
     <AppBar position='sticky' elevation={0}>
         <Toolbar>
             <IconButton
                 size='large'
                 edge='start'
+                onClick={openSideMenu}
             >
                 <MenuOutlined />
             </IconButton>
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,16 +1,19 @@
 import { InboxOutlined, MailOutlineOutlined } from '@mui/icons-material'
+import { useContext } from 'react'
 import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material'
+import { UIContext } from '../../context/ui'
 
 const menuItems: string[] = ['Inbox', 'Stanrred', 'Send Email', 'Draft']
 
 export const Sidebar = () => {
 
+    const { sidemenuOpen, closeSideMenu } = useContext(UIContext);
 
     return (
         <Drawer
             anchor='left'
-            open={true}
-            onClose={() => console.log('cerrando')}
+            open={sidemenuOpen}
+            onClose={closeSideMenu}
         >
             <Box sx={{ width: 250 }}>
                 <Box sx={{ padding: '5px 10px' }}>
